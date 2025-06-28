0ff412d389da186cabdf69fcfcbca25868f2e5d8
Homoeoschool <homoeoschool@gmail.com>
2025-06-28 17:06:15 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import sqlite3
 import json
 
 app = Flask(__name__)
+CORS(app)
+
 
 # Initialize SQLite database (creates sales.db if not present)
 def init_db():
@@ -84,3 +87,4 @@ def checkout_get_info():
 
 if __name__ == "__main__":
     app.run(debug=True)
+	
